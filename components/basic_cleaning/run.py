792fe5d9ee6728aacb478f355369ed7a0305fdd4
Alec Heffron <alecrheffron@gmail.com>
2026-02-26 11:45:38 -0600

    Filter out listings outside NYC boundaries 2

--- components/basic_cleaning/run.py
+++ components/basic_cleaning/run.py
@@ -27,6 +27,9 @@ def go(args):
     df = df.dropna(subset=["price"])
     df = df[(df["price"] >= args.min_price) & (df["price"] <= args.max_price)]
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save cleaned
     out_path = "clean_sample.csv"
     df.to_csv(out_path, index=False)
